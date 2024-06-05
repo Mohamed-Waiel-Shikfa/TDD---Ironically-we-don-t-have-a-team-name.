259b35dbbf4e6d8e083830b8b74f656ce5c2b8e3
Abdulrahman Al-Taweel <134710122+Aboud04@users.noreply.github.com>
2024-06-05 12:01:16 +0300

    Updated test_calculator.py
    first amount of test cases for many numbers

--- test_calculator.py
+++ test_calculator.py
@@ -11,7 +11,7 @@ Test Module for String Calculator
 
 import pytest
 from calculator import add
-
+import random
 
 
 def test_basic_add():
@@ -22,3 +22,18 @@ def test_basic_add():
     assert add("") == "0", "Failed on empty string"
     assert add("1.1, 2.2") == "3.3", "Failed on 1.1+2.2==3.3"
     assert add("1,6,7") == "14", "Failed on 1+6+7==14"
+    
+def many_numbers():
+    for i in range(15):
+        val1= random.randint(0,5)
+        val2 = random.randint(0,5)
+        val3= random.randint(0,5)
+        val4 = random.randint(0,5)
+        val5 = random.randint(0,5)
+        total = val1+val2+val3
+        total1 = val1+val2+val3+val4
+        total2 = val1+val2+val3+val4+val5
+        assert add(f"{val1},{val2},{val3}") == f"{total}", f"Failed on {val1} + {val2} + {val3} == {total}"
+        assert add(f"{val1},{val2},{val3},{val4}") == f"{total}", f"Failed on {val1} + {val2} + {val3} + {val4} == {total1}"
+        assert add(f"{val1},{val2},{val3},{val4},{val5}") == f"{total}", f"Failed on {val1} + {val2} + {val3} + {val4} + {val5} == {total2}"
+    
