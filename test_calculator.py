bf8103d2b164740f928de7bb00ff529059a134ab
relghali <143871826+relghali@users.noreply.github.com>
2024-06-05 12:12:58 +0300

    Update test_calculator.py
    Refactored test cases for many numbers

--- test_calculator.py
+++ test_calculator.py
@@ -23,17 +23,16 @@ def test_basic_add():
     assert add("1.1, 2.2") == "3.3", "Failed on 1.1+2.2==3.3"
     assert add("1,6,7") == "14", "Failed on 1+6+7==14"
     
-def many_numbers():
-    for i in range(15):
-        val1= random.randint(0,5)
-        val2 = random.randint(0,5)
-        val3= random.randint(0,5)
-        val4 = random.randint(0,5)
-        val5 = random.randint(0,5)
-        total = val1+val2+val3
-        total1 = val1+val2+val3+val4
-        total2 = val1+val2+val3+val4+val5
-        assert add(f"{val1},{val2},{val3}") == f"{total}", f"Failed on {val1} + {val2} + {val3} == {total}"
-        assert add(f"{val1},{val2},{val3},{val4}") == f"{total}", f"Failed on {val1} + {val2} + {val3} + {val4} == {total1}"
-        assert add(f"{val1},{val2},{val3},{val4},{val5}") == f"{total}", f"Failed on {val1} + {val2} + {val3} + {val4} + {val5} == {total2}"
-    
+def test_many_numbers():
+    for i in range(20):
+        numbers = [ ]
+        lenNumbers = random.randint(0,500)
+        for j in range(lenNumbers):
+            number = random.randint(0,13000)
+            numbers.append(number)
+        input = ""
+        input += f"{num}," for num in numbers[:-1]
+        input += str(numbers[-1])
+        total = sum(numbers)
+        failMessage = input.replace(",", "+")
+        assert add(input) == total, f"Failed on {failMessage} == {total}"
